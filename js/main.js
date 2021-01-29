9fbbb0dc86ed2c3baf39170f27ac05ecd94fc20c
shahnewaz601 <shahnewaz601@gmail.com>
2021-01-30 01:43:30 +0600

    simple update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //Final Function
-function ticketChange(isIncrease){
-    const ticketQuantity = document.getElementById('firstClass-quantity');
+function handleTicketChange(ticket, isIncrease){
+    const ticketQuantity = document.getElementById(ticket + '-quantity');
     const ticketQuantityNumber = parseInt(ticketQuantity.value);
     // const totalQuantity = ticketQuantityNumber - 1;
     let totalQuantity = ticketQuantityNumber;
@@ -11,9 +11,32 @@ function ticketChange(isIncrease){
         totalQuantity = ticketQuantityNumber - 1;
     }
     ticketQuantity.value = totalQuantity;
-    //For ticket price
-    const totalTicketPrice = totalQuantity * 150;
-    document.getElementById('sub-total').innerText = totalTicketPrice;
+    // //For ticket price
+    // const totalTicketPrice = totalQuantity * 150;
+    // document.getElementById('sub-total').innerText = totalTicketPrice;
+    totalCalculate();
+}
+
+function totalCalculate(){
+    const firstClassInput = document.getElementById('firstClass-quantity');
+    const firstClassInputNumber = parseInt(firstClassInput.value);
+
+    const economyInputNumber = getInputValue('economy');
+
+    const subTotalPrice = firstClassInputNumber * 150 + economyInputNumber * 100;
+    document.getElementById('sub-total').innerText = subTotalPrice;
+
+    const tax = Math.round(subTotalPrice * 0.1);
+    document.getElementById('tax-amount').innerText = tax;
+
+    const total = subTotalPrice + tax;
+    document.getElementById('total-amount').innerText = total;
+}
+
+function getInputValue(ticket){
+    const ticketInput  = document.getElementById(ticket + '-quantity');
+    const ticketInputNumber = parseInt(ticketInput.value);
+    return ticketInputNumber;
 }
 
 
