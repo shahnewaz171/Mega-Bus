a37e7dd8328f8986f30caf828f5edf668004efef
shahnewaz601 <shahnewaz601@gmail.com>
2021-01-30 00:56:25 +0600

    simple update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,75 @@
-//For Increase
-document.getElementById('firstClass-increase').addEventListener('click', function(){
+//Final Function
+function ticketChange(isIncrease){
     const ticketQuantity = document.getElementById('firstClass-quantity');
     const ticketQuantityNumber = parseInt(ticketQuantity.value);
-    const totalQuantity = ticketQuantityNumber + 1;
+    // const totalQuantity = ticketQuantityNumber - 1;
+    let totalQuantity = ticketQuantityNumber;
+    if(isIncrease == true){
+        totalQuantity = ticketQuantityNumber + 1;
+    }
+    if(isIncrease ==  false && totalQuantity > 0){
+        totalQuantity = ticketQuantityNumber - 1;
+    }
     ticketQuantity.value = totalQuantity;
     //For ticket price
     const totalTicketPrice = totalQuantity * 150;
     document.getElementById('sub-total').innerText = totalTicketPrice;
-});
+}
+
+
+
+// //Use function
+// document.getElementById('firstClass-increase').addEventListener('click', function(){
+//   ticketChange(true);
+// });
+
+// document.getElementById('firstClass-decrease').addEventListener('click', function(){
+//     ticketChange(false);
+// });
+
+
+// function ticketChange(isIncrease){
+//     const ticketQuantity = document.getElementById('firstClass-quantity');
+//     const ticketQuantityNumber = parseInt(ticketQuantity.value);
+//     // const totalQuantity = ticketQuantityNumber - 1;
+//     let totalQuantity = ticketQuantityNumber;
+//     if(isIncrease == true){
+//         totalQuantity = ticketQuantityNumber + 1;
+//     }
+//     if(isIncrease ==  false && totalQuantity > 0){
+//         totalQuantity = ticketQuantityNumber - 1;
+//     }
+//     ticketQuantity.value = totalQuantity;
+//     //For ticket price
+//     const totalTicketPrice = totalQuantity * 150;
+//     document.getElementById('sub-total').innerText = totalTicketPrice;
+// }
+
+
+
+
+
+
+// //For Increase
+// document.getElementById('firstClass-increase').addEventListener('click', function(){
+//     const ticketQuantity = document.getElementById('firstClass-quantity');
+//     const ticketQuantityNumber = parseInt(ticketQuantity.value);
+//     const totalQuantity = ticketQuantityNumber + 1;
+//     ticketQuantity.value = totalQuantity;
+//     //For ticket price
+//     const totalTicketPrice = totalQuantity * 150;
+//     document.getElementById('sub-total').innerText = totalTicketPrice;
+// });
+
+
+// //For Decrease
+// document.getElementById('firstClass-decrease').addEventListener('click', function(){
+//     const ticketQuantity = document.getElementById('firstClass-quantity');
+//     const ticketQuantityNumber = parseInt(ticketQuantity.value);
+//     const totalQuantity = ticketQuantityNumber - 1;
+//     ticketQuantity.value = totalQuantity;
+//     //For ticket price
+//     const totalTicketPrice = totalQuantity * 150;
+//     document.getElementById('sub-total').innerText = totalTicketPrice;
+// });
+
