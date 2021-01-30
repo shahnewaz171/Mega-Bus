2c94e52c8e8eab0f699573baf243c3e6965368b6
shahnewaz601 <shahnewaz601@gmail.com>
2021-01-30 19:32:34 +0600

    simple update

--- js/main.js
+++ js/main.js
@@ -65,3 +65,12 @@ function submitInformation(){
     document.getElementById('booking-information').style.display = 'none';
     document.getElementById('ticket-information').style.display = 'block';
 }
+
+
+//For Mobile navBar
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
